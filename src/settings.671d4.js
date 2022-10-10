aa393283ccd145f3b3bc4f52c53aae0ec2fb444d
souloss <lebowitzgerald407@gmail.com>
2022-10-10 22:54:13 +0800

    first commit

---
+++ src/settings.671d4.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","BG","UI"],collisionMatrix:[[true],[false,false],[false,false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/MainGameScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",main:"b374a"}};
